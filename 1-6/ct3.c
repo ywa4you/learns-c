71706b684da5e990a51da0bd6736bb214dea3d6a
ywa <5ab81c2babf2aabc9985122d2ef11aaf@tutanota.com>
2023-04-12 09:38:35 +0200

    Refactor project and Add exercises frome day 3
    Having noticed that the Exexcises are formated like 1-6 in the book,
    unlike the chapters 1.6 I have renamed the exercise folders
    accordingly.
    
    This commit also solves the exercises 1-6 and 1-7 since the last commit
    didn't.

---
+++ 1-6/ct3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* copy input to output; 2nd version */
+main()
+{
+    int c;
+    c = getchar() != EOF;
+    printf("%d\n", c);
+}
---
+++ 1-7/ct.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* print the value of EOF, first it's contens as a character, then it's value as an integer.
+ * I've done both, as I was unsure of "which" value exercise 1-7 refered to */
+main()
+{
+    putchar(EOF);
+    printf("\n");
+    printf("%d\n", EOF);
+}
